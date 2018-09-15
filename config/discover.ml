8887cf579ddccfc9a3052348b72e8f1204c3de76
Bryan Phelps <bryphe@outlook.com>
2018-09-15 12:40:14 -0700

    Refactor flags to cxx_flags

--- config/discover.ml
+++ config/discover.ml
@@ -1,3 +1,30 @@
-print_endline "Hello from discover!";
 
-Configurator.V1.Flags.write_sexp "c_flags.sexp" ["-I ./../../../include"];
+type os =
+    | Windows
+    | Mac
+    | Linux (* TODO *)
+    | Unknown
+
+let uname () =
+    let ic = Unix.open_process_in "uname" in
+    let uname = input_line ic in
+    let () = close_in ic in
+    uname;;
+
+let get_os = 
+    match Sys.os_type with
+    | "Win32" -> Windows
+    | _ -> match uname () with
+        | "Darwin" -> Mac
+        | _ -> Unknown
+
+let c_flags = ["-I";  "./../../../include"]
+
+let cxx_flags =
+    match get_os with
+    | Windows -> c_flags @ ["-fno-exceptions"; "-fno-rtti"; "-lstdc++"]
+    | _ -> c_flags
+;;
+
+Configurator.V1.Flags.write_sexp "c_flags.sexp" c_flags;
+Configurator.V1.Flags.write_sexp "cxx_flags.sexp" cxx_flags;
