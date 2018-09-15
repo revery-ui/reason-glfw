5941ad30b6a2441d112f0c2e079305179319453e
Bryan Phelps <bryphe@outlook.com>
2018-09-15 12:49:34 -0700

    Generalize flags/cxx_flags

--- config/discover.ml
+++ config/discover.ml
@@ -20,6 +20,24 @@ let get_os =
 
 let c_flags = ["-I";  "./../../../include"]
 
+let ccopt s = ["-ccopt"; s]
+let cclib s = ["-cclib"; s]
+
+let flags =
+    match get_os with
+    | Windows ->  []
+        @ ccopt("-L./../../lib-mingw-w64")
+        @ cclib("-lglfw3")
+        @ cclib("-lgdi32")
+    | _ -> []
+        @ ccopt("-L./../../glfw/src")
+        @ cclib("-lglfw3")
+        @ ccopt("-framework OpenGL")
+        @ ccopt("-framework Cocoa")
+        @ ccopt("-framework IOKit")
+        @ ccopt("-framework CoreVideo")
+;;
+
 let cxx_flags =
     match get_os with
     | Windows -> c_flags @ ["-fno-exceptions"; "-fno-rtti"; "-lstdc++"]
@@ -28,3 +46,4 @@ let cxx_flags =
 
 Configurator.V1.Flags.write_sexp "c_flags.sexp" c_flags;
 Configurator.V1.Flags.write_sexp "cxx_flags.sexp" cxx_flags;
+Configurator.V1.Flags.write_sexp "flags.sexp" flags;
