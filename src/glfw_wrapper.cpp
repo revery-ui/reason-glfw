5f3e2603148fb94eb9a4f505481cf2121c370845
Akin909 <akin.sowemimo@gmail.com>
2019-02-06 20:39:43 +0000

    Add get monitor size to gflw wrapper

--- src/glfw_wrapper.cpp
+++ src/glfw_wrapper.cpp
@@ -543,6 +543,23 @@ extern "C" {
     }
 
     CAMLprim value
+    caml_glfwGetPrimaryMonitorPhysicalSize(value vMonitor)
+    {
+        CAMLparam1(vMonitor);
+        CAMLlocal1(size)
+        GLFWmonitor* pMonitor = (GLFWmonitor*)vMonitor;
+
+        int widthMM, heightMM;
+        glfwGetMonitorPhysicalSize(monitor, &widthMM, &heightMM);
+
+        ret = caml_alloc(2, 0)
+        Store_field(size, 0, Val_int(widthMM));
+        Store_field(size, 0, Val_int(heightMM));
+
+        CAMLreturn(ret);
+    }
+
+    CAMLprim value
     caml_glfwSetCursorPosCallback(value vWindow, value vCallback) {
         CAMLparam2(vWindow, vCallback);
 
